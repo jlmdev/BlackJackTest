68ebf38c21b43ce7849c79c49d585c21d14de5ed
Josh Mann <jlmann72@gmail.com>
2020-08-14 08:00:19 -0400

    Created Method to assign values based on face in Card Class

--- Program.cs
+++ Program.cs
@@ -7,7 +7,40 @@ namespace blackjack
     {
         public string Suit { get; set; }
         public string Face { get; set; }
-        public int Value { get; set; }
+        public int Value()
+        {
+            switch (Face)
+            {
+                case "Ace":
+                    return 11;
+                case "2":
+                    return 2;
+                case "3":
+                    return 3;
+                case "4":
+                    return 4;
+                case "5":
+                    return 5;
+                case "6":
+                    return 6;
+                case "7":
+                    return 7;
+                case "8":
+                    return 8;
+                case "9":
+                    return 9;
+                case "10":
+                    return 10;
+                case "Jack":
+                    return 10;
+                case "Queen":
+                    return 10;
+                case "King":
+                    return 10;
+                default:
+                    return 0;
+            }
+        }
     }
 
     class Program
@@ -40,11 +73,13 @@ namespace blackjack
             }
 
             // Test printing out all of the cards
+            // IMPORTANT!! reference variable.property representing instance of the class's property directly
+            // Results in CS0120: An object reference is required ... error otherwise
 
-            foreach (var cardName in deck)
-            {
-                Console.WriteLine($"{cardName.Face} of {cardName.Suit}");
-            }
+            // foreach (var cardName in deck)
+            // {
+            //     Console.WriteLine($"{cardName.Face} of {cardName.Suit} with value of {cardName.Value()}");
+            // }
         }
     }
 }
